1801426775c4de25f0169332be4115bdd2f64aa4
Austin <austin.dev653@gmail.com>
2019-10-10 16:23:03 -0500

    wip

---
+++ public/component---src-pages-about-js-24ff58fe9df2fb56345a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{224:function(c,e,n){"use strict";n.r(e);var t=n(1),o=(n(0),n(88)),u=n(221);e.default=function(){return Object(t.c)(u.a,null,Object(t.c)("h1",null,"About Me"),Object(t.c)("p",null,"This is my personal website."),Object(t.c)(o.Link,{to:"/"},"← back to home"))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-24ff58fe9df2fb56345a.js.map
