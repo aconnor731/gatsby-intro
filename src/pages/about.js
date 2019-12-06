e3a8cee2e58b1f4c51ab014c0e7eb7f55e275802
Austin <austin.dev653@gmail.com>
2019-12-06 12:59:11 -0600

    add fun facts

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,5 +6,12 @@ export default () => (
     <Layout>
       <h1>About Me</h1>
       <p>This is my personal website.</p>
+      <h2>Fun Facts About Me:</h2>
+      <ul>
+        <li>
+          Favorite movie is The 40 Year Old Virgin or pretty much
+          anything by Judd Apatow.
+        </li>
+      </ul>
     </Layout>
 )
