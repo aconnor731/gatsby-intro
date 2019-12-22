f083d03019bba8e8d9678fd063f7a3519c18be79
Austin <austin.dev653@gmail.com>
2019-12-22 12:10:17 -0600

    fix previous commit

--- src/components/header.js
+++ src/components/header.js
@@ -37,7 +37,6 @@ const Header = () => (
         }
       `}
     >
-        <NavLink to="/" fontWeight={"bold"}>Austin Connor</NavLink>
         <nav>
             {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
               <path d="M2 6h20v3H2zm0 5h20v3H2zm0 5h20v3H2z"></path>
